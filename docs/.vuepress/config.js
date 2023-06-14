8752695eac047731be5d986cba253aaa31fc8288
smori <shinichiro.mori.1983@gmail.com>
2023-06-14 20:28:42 +0900

    Init site

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  base: '/vuepress-plugin-flexsearch-demo/',
+  title: 'vuepress-plugin-flexsearch Demo',
+  dest: 'docs/.vuepress/dist',
+
+  locales: {
+    '/': {
+      lang: 'ja-JP',
+    },
+    '/en/': {
+      lang: 'en-US',
+    },
+  },
+
+  themeConfig: {
+    searchPlaceholder: 'Search ( s, / )',
+
+    locales: {
+      '/': {
+        sidebar: [
+        ],
+      },
+      '/en/': {
+        sidebar: [
+        ],
+      },
+    },
+  },
+
+  plugins: [
+  ],
+
+  markdown: {
+    extendMarkdown: (md) => {
+      md.set({ breaks: true });
+    },
+  },
+};
