a8091be9f84112a93cf050e3ad736c13bb10eb8e
smori <shinichiro.mori.1983@gmail.com>
2026-08-16 15:11:20 +0900

    Reimplement sidebar.js as class

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,68 +1,85 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-/**
- * @param {string} basePath
- * @param {Object.<string, {lang: string}>} locales
- * @param {string} target
- * @return {string[]}
- */
-const collect = (basePath, locales, target) => {
-  const localeBasePath = getLocaleBasePath(basePath, target);
+class Sidebar {
+  /**
+   * @param {string} basePath
+   * @param {Object.<string, {lang: string}>} locales
+   */
+  constructor(basePath, locales) {
+    /**
+     * @type {string}
+     * @private
+     */
+    this._basePath = basePath;
 
-  const excludeDirectories = getExcludeDirectories(locales, target);
+    /**
+     * @type {Object.<string, {lang: string}>}
+     * @private
+     */
+    this._locales = locales;
+  }
 
-  const targetDirectories = fs.readdirSync(localeBasePath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .filter(dirent => dirent.name !== '.vuepress')
-    .filter(dirent => {
-      if (target === '/') {
-        return excludeDirectories.includes(dirent.name) === false;
-      } else {
-        return true;
-      }
-    })
-    .map(dirent => dirent.name)
-    .sort();
+  /**
+   * @param {string} target
+   * @return {string[]}
+   */
+  collect(target) {
+    const localeBasePath = this._getLocaleBasePath(target);
+    const excludeDirectories = this._getExcludeDirectories(target);
 
-  return targetDirectories
-    .map((dir) => {
-      return fs.readdirSync(path.join(localeBasePath, dir), { withFileTypes: true })
-        .filter(dirent => dirent.isFile())
-        .filter(dirent => /\.md/.test(dirent.name))
-        .map(dirent => path.join(target, dir, dirent.name))
-        .sort();
-    })
-    .reduce((prev, current) => {
-      return prev.concat(current);
-    }, []);
+    const targetDirectories = fs.readdirSync(localeBasePath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .filter(dirent => dirent.name !== '.vuepress')
+      .filter(dirent => {
+        if (target === '/') {
+          return excludeDirectories.includes(dirent.name) === false;
+        } else {
+          return true;
+        }
+      })
+      .map(dirent => dirent.name)
+      .sort();
+
+    return targetDirectories
+      .map((dir) => {
+        return fs.readdirSync(path.join(localeBasePath, dir), { withFileTypes: true })
+          .filter(dirent => dirent.isFile())
+          .filter(dirent => /\.md/.test(dirent.name))
+          .map(dirent => path.join(target, dir, dirent.name))
+          .sort();
+      })
+      .reduce((prev, current) => {
+        return prev.concat(current);
+      }, []);
+  }
+
+  /**
+   * @param {string} target
+   * @return {string}
+   * @private
+   */
+  _getLocaleBasePath(target) {
+    return `${this._basePath}/${target}`.replace(/\/+/g, '/');
+  }
+
+  /**
+   * @param {string} target
+   * @return {string[]}
+   * @private
+   */
+  _getExcludeDirectories(target) {
+    //
+    // If target is root, directories of other locales should be excluded.
+    //
+    if (target === '/') {
+      return Object.keys(this._locales)
+        .filter(localeBasePath => localeBasePath !== target)
+        .map(localeBasePath => localeBasePath.replace(/^\/|\/$/g, ''));
+    } else {
+      return [];
+    }
+  }
 }
 
-/**
- * @param {string}  basePath
- * @param {string} target
- * @return {string}
- */
-const getLocaleBasePath = (basePath, target) => {
-  return `${basePath}/${target}`.replace(/\/+/g, '/');
-};
-
-/**
- * @param {Object.<string, {lang: string}>} locales
- * @param {string} target
- * @return {string[]}
- */
-const getExcludeDirectories = (locales, target) => {
-  //
-  // If target is root, directories of other locales should be excluded.
-  //
-  if (target === '/') {
-    return Object.keys(locales)
-      .filter(localeBasePath => localeBasePath !== target)
-      .map(localeBasePath => localeBasePath.replace(/^\/|\/$/g, ''));
-  } else {
-    return [];
-  }
-};
-
-module.exports.collect = collect;
+module.exports = Sidebar;
